5f47775a1c86d74218ee03148d48b6630f834123
vadi <vadz77@hotmail.com>
2024-02-29 03:51:38 +1100

    Add JWT cookie handling and response enhancement to auth status route

--- app/api/auth/status/route.ts
+++ app/api/auth/status/route.ts
@@ -1,11 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getAuthSession } from "@app/auth/session";
+import { APP_SESSION_COOKIE_NAME } from "@lib/constants";
+import { decodeToken } from "@lib/tokens";
 
 export async function GET(request: NextRequest) {
-  const authSession = getAuthSession();
+  const jwtCookie = request.cookies.get(APP_SESSION_COOKIE_NAME);
+
+  if ( !jwtCookie || !jwtCookie?.value ) {
+    return NextResponse.json(
+      { error: "Session not found" },
+      {
+        status: 404
+      }
+    );
+  }
+
+  const authSession = await getAuthSession();
+  const decodedCookie: any = decodeToken(jwtCookie.value);
+
+  delete decodedCookie.iat;
+  delete decodedCookie.exp;
+  delete decodedCookie.id;
 
   return NextResponse.json(
-    { authSession },
+    { 
+      authSession,
+      decoded: decodedCookie
+    },
     {
       status: 200
     }
