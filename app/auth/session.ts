890c174ac5f0da43768790b55824552c6d7b8db4
vadi <vadz77@hotmail.com>
2024-03-01 10:13:18 +1100

    feat: Implement user profile management and update task creation
     • Added user profile management with form and update action.
     • Integrated profile data into the session and side navigation display.
     • Refined task creation form and removed unused imports.
     • Introduced new UI components for input fields and tooltips.
     • Updated Prisma schema with profile fields and created a new migration.
     • Added new Radix UI packages for select and tooltip components.

--- app/auth/session.ts
+++ app/auth/session.ts
@@ -1,12 +1,14 @@
 "use server"
 import { cookies } from 'next/headers';
 import db from "@lib/db";
-import { type User, type ActiveSession } from "@prisma/client";
+import { type User, type Profile, type ActiveSession } from "@prisma/client";
 import { APP_SESSION_COOKIE_NAME } from '@lib/constants';
 import { Logger } from '@lib/logger';
 
 export type AuthSessionResponse = Pick<ActiveSession, 'expiresAt'> & {
-  user: Pick<User, 'email' | 'role' | 'id'>;
+  user: Pick<User, 'email' | 'role' | 'id'> & {
+    profile: Profile | null;
+  };
 };
 
 export const getAuthSession = async (): Promise<AuthSessionResponse | null> => {
@@ -27,6 +29,7 @@ export const getAuthSession = async (): Promise<AuthSessionResponse | null> => {
             id: true,
             email: true,
             role: true,
+            profile: true,
           },
         },
       },
