f87ce0aceef6ec62988f11f6ca546ec4a85166c3
vadi <vadz77@hotmail.com>
2024-02-29 19:00:03 +1100

    Update MagicLoginSchema to enforce email length constraints
     - Add minimum length validation (5 characters) with a custom message.
     - Add maximum length validation (128 characters) with a custom message.
     - Modify email validation error message for clarity.

--- schemas/auth.ts
+++ schemas/auth.ts
@@ -1,7 +1,9 @@
 import * as z from "zod";
 
 export const MagicLoginSchema = z.object({
-  email: z.string().email({
-    message: "Email is required",
-  })
+  email: z
+    .string()
+    .min(5, { message: "This field has to be filled." })
+    .max(128, { message: "This field has to be filled." })
+    .email("This is not a valid email.")
 });
