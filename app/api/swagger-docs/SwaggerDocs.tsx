bf62f35d47f708fd7e0af92bf365b91ec9de0cab
vadi <vadz77@hotmail.com>
2024-03-02 06:04:31 +1100

    Update SwaggerDocs Component
    The commit includes the following changes:
    
     • Updated the import statement for SwaggerUI to use dynamic import from next/dynamic.
     • Created a dynamic version of SwaggerUI component using dynamic function from next/dynamic.
     • Updated the component to use the dynamic version of SwaggerUI component.
    
    These changes improve the performance of the SwaggerDocs component by using dynamic import for the SwaggerUI component.

--- app/api/swagger-docs/SwaggerDocs.tsx
+++ app/api/swagger-docs/SwaggerDocs.tsx
@@ -1,13 +1,18 @@
 'use client';
 
-import SwaggerUI from 'swagger-ui-react';
+import dynamic from "next/dynamic";
+
+const DynamicSwaggerUI = dynamic(() => import("swagger-ui-react"), {
+  ssr: false,
+  loading: () => <p>Loading Component...</p>,
+});
 
 type ReactSwaggerProps = {
   spec: Record<string, any>,
 };
 
 const ReactSwagger: React.FC<ReactSwaggerProps> = props => {
-  return <SwaggerUI spec={props.spec} />;
+  return <DynamicSwaggerUI spec={props.spec} />;
 }
 
 export default ReactSwagger;
