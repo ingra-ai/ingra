8eb4a1d4dc7a15f49d2015c972c526a9f5fd0434
vadi <vadz77@hotmail.com>
2024-03-02 00:28:08 +1100

    Refactor Swagger Documentation Configuration and Route
    The commit includes the following changes:
    
     • Added a new config.ts file in the app/api/swagger directory to store Swagger configuration options.
     • Created a swaggerOptions object to define the Swagger configuration.
     • Created a getSwaggerSpec function to generate the Swagger specification using the swaggerOptions.
     • Added a new route.ts file in the app/api/swagger/docs directory to handle the GET request for the Swagger specification.
     • Updated the page.tsx file in the app/api/swagger directory to import the getSwaggerSpec function from the config.ts file and
       use it to retrieve the Swagger specification.
    
    These changes refactor the Swagger documentation configuration and route, making it more modular and easier to maintain.

--- app/api/swagger/page.tsx
+++ app/api/swagger/page.tsx
@@ -1,40 +1,14 @@
-import { APP_NAME } from '@lib/constants';
-import { createSwaggerSpec } from 'next-swagger-doc';
 import ReactSwagger from './SwaggerDocs';
+import { getSwaggerSpec } from '@/app/api/swagger/config';
 
 import '@css/swagger.scss';
 
-const getApiDocs = async () => {
-  const spec = createSwaggerSpec({
-    apiFolder: 'app/api', // define api folder under app folder
-    definition: {
-      openapi: '3.0.0',
-      info: {
-        title: `${ APP_NAME } API`,
-        version: '1.0',
-      },
-      components: {
-        securitySchemes: {
-          BearerAuth: {
-            type: 'http',
-            scheme: 'bearer',
-            bearerFormat: 'JWT',
-          },
-        },
-      },
-      security: [],
-    },
-  });
-  return spec;
-};
-
-
 export default async function Page() {
-  const spec = await getApiDocs();
+  const swaggerSpec = await getSwaggerSpec();
 
   return (
     <section className="container">
-      <ReactSwagger spec={spec} />
+      <ReactSwagger spec={swaggerSpec} />
     </section>
   );
 }
