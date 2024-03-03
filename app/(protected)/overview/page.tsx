a69b5d46e27c52612f5005af25ad6c6a8608d43d
vadi <vadz77@hotmail.com>
2024-03-04 01:28:20 +1100

    Refactor code: Added link to GPT Plugins settings, improved styling, and updated note about phrase code expiration.

--- app/(protected)/overview/page.tsx
+++ app/(protected)/overview/page.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
 import { CSSProperties } from "react";
 
 export default async function Dashboard() {
   return (
-    <div>
-      Overview
+    <div className="leading-7">
+      <h1 className="text-lg">Setting up to use GPT Plugins</h1>
+      <p>Go to your <Link className="underline font-medium" href='/settings/gpt-plugins' prefetch={true}>GPT Plugins Settings</Link></p>
     </div>
   );
 }
