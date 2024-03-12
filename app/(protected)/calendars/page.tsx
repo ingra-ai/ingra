5756b8c8eb842cbd33ef8aaab35fe79b22710988
vadi <vadz77@hotmail.com>
2024-03-12 10:00:51 +0000

    The provided diff shows two sets of changes:
     1 In the file page.tsx, a new paragraph has been added that mentions the option to remove access at https://myaccount.google.com/connections. This change
       is related to displaying a message to the user.
     2 A new file vercel.json has been added. It contains a configuration for a cron job that runs every 30 minutes. The cron job executes the script
       /api/cron/google-oauth-refresh. This change is related to scheduling a task for refreshing Google OAuth access tokens.

--- app/(protected)/calendars/page.tsx
+++ app/(protected)/calendars/page.tsx
@@ -6,6 +6,7 @@ export default async function Calendar() {
   return (
     <div className="leading-7">
       <p> Coming soon </p>
+      <p> You can always remove your access at <a href="https://myaccount.google.com/connections" target="_blank">https://myaccount.google.com/connections</a> </p>
       <GoogleButtonUI 
         type="redirect"
         text="Google Calendar Connect" 
