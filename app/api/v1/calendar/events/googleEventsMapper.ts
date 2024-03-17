b389b1503d5503a0c8bd90daf878ee371d1b4fe8
vadi <vadz77@hotmail.com>
2024-03-17 11:27:17 +0000

    Added google response mock data

--- app/api/v1/calendar/events/googleEventsMapper.ts
+++ app/api/v1/calendar/events/googleEventsMapper.ts
@@ -5,7 +5,7 @@ import { format, utcToZonedTime } from 'date-fns-tz';
 
 export const mapGoogleCalendarEvent = (event: calendar_v3.Schema$Event, timeZone: string): ApiCalendarEvent => {
   // Extracting necessary information
-  const { id, summary, start, end, htmlLink, reminders, attendees, location, creator, organizer } = event;
+  const { id, summary, description, start, end, htmlLink, reminders, attendees, location, creator, organizer } = event;
 
   // Formatting dates
   const startDate = start?.dateTime ? utcToZonedTime(new Date(start.dateTime), timeZone) : null;
@@ -32,7 +32,8 @@ export const mapGoogleCalendarEvent = (event: calendar_v3.Schema$Event, timeZone
 
   return {
     eventId: id || 'N/A',
-    title: summary || 'N/A',
+    summary: summary || 'N/A',
+    description: description || 'N/A',
     creator: creator?.self ? 'me' : creator?.email || 'N/A',
     organizer: organizer?.self ? 'me' : organizer?.email || 'N/A',
     startDateTime: formattedStartDate || 'N/A',
