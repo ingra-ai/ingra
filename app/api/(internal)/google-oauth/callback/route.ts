6638ac9d538400ce0e99abdcc49e5c063396d493
vadi <vadz77@hotmail.com>
2024-03-17 04:06:30 +0000

    Add more scopes to google apis - Calendar, COntacts, Gmail, GDrive, Sheets and People API

--- app/api/(internal)/google-oauth/callback/route.ts
+++ app/api/(internal)/google-oauth/callback/route.ts
@@ -23,29 +23,55 @@ export async function GET(request: NextRequest) {
   if (type === 'redirect') {
     const authUrl = oauth2Client.generateAuthUrl({
       access_type: 'offline',
-
-      /**
-       * @see https://developers.google.com/calendar/api/auth#calendar-api-scopes
-       */
       scope: [
+        // Basic profile information, to identify the user
         'https://www.googleapis.com/auth/userinfo.email',
         'https://www.googleapis.com/auth/userinfo.profile',
+    
+        // Full access to Google Calendar, to manage events and settings
         'https://www.googleapis.com/auth/calendar',
-
-        // See and download any calendar you can access using your Calendar.
-        // 'https://www.googleapis.com/auth/calendar.readonly',
-
-        // View and edit events on all your calendars.
-        // 'https://www.googleapis.com/auth/calendar.events',
-
-        // View events on all your calendars.
-        // 'https://www.googleapis.com/auth/calendar.events.readonly',
-
-        // View your Calendar settings.
-        // 'https://www.googleapis.com/auth/calendar.settings.readonly',
-
-        // Run as a Calendar add-on.
-        // 'https://www.googleapis.com/auth/calendar.addons.execute',
+    
+        // Access to Google Contacts, to view and manage contacts
+        'https://www.googleapis.com/auth/contacts',
+    
+        // Full access to Gmail, to view and send emails, manage mail settings
+        'https://www.googleapis.com/auth/gmail.modify',
+    
+        // View your emails and settings
+        // 'https://www.googleapis.com/auth/gmail.readonly',
+    
+        // Send emails
+        // 'https://www.googleapis.com/auth/gmail.send',
+    
+        // Manage your basic mail settings
+        // 'https://www.googleapis.com/auth/gmail.settings.basic',
+    
+        // Manage your sensitive mail settings, including forwarding rules and aliases
+        // 'https://www.googleapis.com/auth/gmail.settings.sharing',
+    
+        // Full access to Google Drive, to view, edit, and organize drive files
+        'https://www.googleapis.com/auth/drive',
+    
+        // View and manage Google Drive files and folders that you have opened or created with this app
+        // 'https://www.googleapis.com/auth/drive.file',
+    
+        // View Google Drive files and folders that are shared with you
+        // 'https://www.googleapis.com/auth/drive.readonly',
+    
+        // Access to Google Sheets, to view and manage sheets
+        'https://www.googleapis.com/auth/spreadsheets',
+    
+        // Read-only access to Google Sheets
+        // 'https://www.googleapis.com/auth/spreadsheets.readonly',
+    
+        // Access to user's contacts via the People API, to view and manage the contacts
+        'https://www.googleapis.com/auth/contacts',
+    
+        // Access to view your contacts, their photos, and the people most important to you
+        // 'https://www.googleapis.com/auth/contacts.readonly',
+    
+        // Access to view information about your Google account
+        // 'https://www.googleapis.com/auth/plus.login',
       ],
     });
 
