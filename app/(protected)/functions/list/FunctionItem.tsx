a223efc69fa3da218adbd082a54ab6f1b0395873
vadi <vadz77@hotmail.com>
2024-05-20 13:37:05 +0000

    This update introduces several changes to the codebase:
     1 The project roadmap in the README.md file has been modified. A new task, "Self Function Generation," has been added to provide an API endpoint for users to curate their
       own functions using GPT.
     2 The FunctionForm component has been updated. It now includes a button to clone a function when in edit mode. Cloning creates a new function with the same values as the
       existing function, except for a modified slug and privacy settings.
     3 The upsertFunction function in the functions.ts file has been modified to handle the new "originalFunctionId" field. This field is used to track the original function
       when cloning.
     4 The FunctionItem component has been updated to display additional information about a function, including its privacy status, tags, and the number of arguments it has.
     5 Two new migration files have been added to update the function schema in the database. These migrations modify the "originalFunctionId" column to change its data type
       from TEXT to VARCHAR(64).
    
    Overall, these changes enhance the functionality and user experience of the application by adding self function generation and cloning capabilities.

--- app/(protected)/functions/list/FunctionItem.tsx
+++ app/(protected)/functions/list/FunctionItem.tsx
@@ -34,28 +34,42 @@ interface FunctionItemProps {
 const FunctionItem: React.FC<FunctionItemProps> = (props) => {
   const { functionData, onEdit, onDelete } = props;
   const subtext = [functionData.httpVerb, functionData.isPublished ? 'Published' : 'Unpublished'].filter(Boolean).join(' - ');
-  const description = (functionData.description || '~').slice(0, 200);
+  const totalArguments = functionData.arguments.length;
   return (
-    <div className="bg-secondary border border-gray-500 hover:border-gray-300 shadow-md rounded-sm p-4 w-full min-h-[200px] cursor-pointer">
-      <div className="py-2">
+    <div 
+      className="flex flex-col bg-secondary border border-gray-500 hover:border-gray-300 shadow-md rounded-sm px-4 py-2 w-full min-h-[200px] cursor-pointer"
+      title={functionData.description}
+    >
+      <div className="block space-y-2 py-2 h-full"
+        role='button'
+        tabIndex={0}
+        aria-pressed='false'
+        onClick={onEdit}
+      >
         <p className="text-gray-400 text-xs">{subtext}</p>
         <h2 className="text-lg font-bold text-gray-100 truncate min-w-0" title={functionData.slug}>
           {functionData.slug}
         </h2>
+        <div className="block">
+          <span className={`px-2 py-1 inline-flex text-xs leading-3 font-semibold rounded-full ${functionData.isPrivate ? 'bg-red-700 text-red-200' : 'bg-green-700 text-green-200'}`}>
+            {functionData.isPrivate ? 'Private' : 'Public'}
+          </span>
+          {functionData.tags.map(tag => (
+            <Badge key={tag.id} variant="accent" className="mr-1 text-xs">{tag.name}</Badge>
+          ))}
+        </div>
+        {
+          totalArguments > 0 && (
+            <div className="block">
+              <p className="text-xs text-gray-400">
+                +{totalArguments} argument{totalArguments > 1 ? 's' : ''}
+              </p>
+            </div>
+          )
+        }
       </div>
-      <div className="block">
-        <span className={`px-2 py-1 inline-flex text-xs leading-3 font-semibold rounded-full ${functionData.isPrivate ? 'bg-red-700 text-red-200' : 'bg-green-700 text-green-200'}`}>
-          {functionData.isPrivate ? 'Private' : 'Public'}
-        </span>
-        {functionData.tags.map(tag => (
-          <Badge key={tag.id} variant="accent" className="mr-1 text-xs">{tag.name}</Badge>
-        ))}
-      </div>
-      <div className="block min-h-[175px] py-4">
-        <p className="text-gray-300 text-sm font-medium" title={description}>{description}</p>
-      </div>
-      <div className="mt-auto grid grid-cols-12">
-        <div className="col-span-8 flex justify-start items-center">
+      <div className="grid grid-cols-12">
+        <div className="col-span-8 flex justify-start items-start">
           <TooltipProvider>
             <Tooltip>
               <TooltipTrigger>
