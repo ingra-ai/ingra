754cb225f5195521f9e0c77dfb3f665491dbc175
vadi <vadz77@hotmail.com>
2024-05-21 11:46:01 +0000

    This update introduces several changes to the codebase:
      1 In the functions.ts file, additional logging statements have been added to track the execution of functions. The logs now include the function slug,
        request arguments, and metrics.
      2 The route.ts file now logs the invocation of handlers in the marketplace API.
      3 The functions.ts file in the me directory has been updated with similar logging changes as in the marketplace API.
      4 A new auditLog.ts file has been added to handle logging to an audit table in the database.
      5 The Jest configuration has been modified to include a setup file that mocks the pushToAuditTable function.
      6 The Redis extension now logs cache hits and misses for active sessions and API keys.
      7 The logger has been updated to include a new reporter that pushes logs to the audit table.
      8 Two new migration files have been added to introduce the AuditLog table and modify the tag column length.
      9 The schema.prisma file has been updated to include the AuditLog model.
    
    Overall, these changes enhance logging and introduce an audit log feature to track important events in the system.

--- app/api/v1/marketplace/[[...paths]]/handlers/functions.ts
+++ app/api/v1/marketplace/[[...paths]]/handlers/functions.ts
@@ -3,7 +3,7 @@ import { ActionError } from '@v1/types/api-response';
 import db from "@lib/db";
 import { Logger } from "@lib/logger";
 import { run } from "@app/api/utils/vm/run";
-import type { UserSandboxOutput } from "@app/api/utils/vm/types";
+import type { MetricSandboxOutput, UserSandboxOutput } from "@app/api/utils/vm/types";
 import { apiTryCatch } from "@app/api/utils/apiTryCatch";
 import { getUserIdByProfileName } from "@/data/user";
 import { generateMarketplaceVmArgs } from "./generateMarketplaceVmArgs";
@@ -13,13 +13,13 @@ const handlerFn = async ( requestArgs: Record<string, any> = {}, username: strin
     const [functionSlug] = restOfPaths;
 
     if ( functionSlug ) {
-      Logger.withTag('marketplace-functions').withTag(username).info(`starts executing function: ${functionSlug} - with args: ${JSON.stringify(requestArgs)}`);
-
       const userId = await getUserIdByProfileName(username);
 
       if ( !userId ) {
         throw new ActionError('error', 400, `User not found.`);
       }
+
+      Logger.withTag('marketplace-functions').withTag(userId).info(`starts executing function: ${functionSlug}`, requestArgs);
 
       const functionRecord = await db.function.findUnique({
         where: {
@@ -43,14 +43,20 @@ const handlerFn = async ( requestArgs: Record<string, any> = {}, username: strin
       const vmContext = generateMarketplaceVmArgs(functionRecord.arguments, requestArgs);
 
       const vmOutput = await run(functionRecord.code, vmContext as any);
-
-      const errors = vmOutput.outputs.filter(output => output.type === 'error') as UserSandboxOutput[];
+      const errors = vmOutput.outputs.filter(output => output.type === 'error') as UserSandboxOutput[],
+        metrics = (( vmOutput.outputs || [] ).filter(output => output.type === 'metric') as MetricSandboxOutput[] ).map((metric) => {
+          return {
+            [metric.metric]: metric.value,
+          };
+        });
 
       if ( errors.length ) {
-        throw new ActionError('error', 400, errors[0].message);
+        const errorMessage = errors?.[0].message || 'An error occurred while executing the function.';
+        Logger.withTag('marketplace-functions').withTag(userId).error(`finishes executing function: ${functionSlug}`);
+        throw new ActionError('error', 400, errorMessage);
       }
 
-      Logger.withTag('marketplace-functions').withTag(username).info(`finishes executing function: ${functionSlug}`);
+      Logger.withTag('marketplace-functions').withTag(userId).info(`finishes executing function: ${functionSlug}`, metrics);
 
       return NextResponse.json(
         {
