6fd19694be24bfbf81108b17211ba2fc920ccae0
vadi <vadz77@hotmail.com>
2024-05-21 15:55:23 +0000

    This update includes several changes:
     • Added instructions to the README.md file on how to use the Bakabit Portal, including login, accessing curated functions, generating API keys, and using
       JSON in chatgpt plugins.
     • Updated the Function Marketplace status to indicate that it is now functional, but with the intention to improve it later.
     • Added a function to refresh OAuth tokens in the session.ts file, which checks if the token has expired and refreshes it if necessary.
     • Updated the getUserByJwt function in the user.ts file to return the active session along with the user information.
     • Added an update to the API key's lastUpdatedAt field in the getUserByApiKey function in the user.ts file.

--- app/auth/session.ts
+++ app/auth/session.ts
@@ -1,9 +1,12 @@
 'use server';
 import { cookies, headers } from 'next/headers';
 import { type User, type Profile, type OAuthToken, type EnvVars } from '@prisma/client';
-import { APP_SESSION_API_KEY_NAME, APP_SESSION_COOKIE_NAME } from '@lib/constants';
+import { APP_GOOGLE_OAUTH_CALLBACK_URL, APP_GOOGLE_OAUTH_CLIENT_ID, APP_GOOGLE_OAUTH_CLIENT_SECRET, APP_SESSION_API_KEY_NAME, APP_SESSION_COOKIE_NAME } from '@lib/constants';
 import { Logger } from '@lib/logger';
 import { getUserByApiKey, getUserByJwt } from '@/data/user';
+import db from '@lib/db';
+import { deleteAllUserCaches } from '@lib/db/extensions/redis';
+import { google } from 'googleapis';
 
 export type AuthSessionResponse = {
   expiresAt?: Date;
@@ -41,9 +44,74 @@ export const getAuthSession = async (): Promise<AuthSessionResponse | null> => {
       sessionWithUser = await getUserByApiKey(xApiKey);
     }
 
+    if ( sessionWithUser && (sessionWithUser.user?.oauthTokens || [])?.length > 0 ) {
+      await Promise.all( sessionWithUser.user.oauthTokens.map( refreshOauthToken ) );
+    }
+
     return sessionWithUser;
   } catch (error) {
     Logger.withTag('AuthSession').error('Error fetching user and session by either JWT or API key:', error);
     return null;
   }
 };
+
+export const refreshOauthToken = async (oauthToken: OAuthToken) => {
+  const currentDate = new Date();
+
+  if ( oauthToken.expiryDate && currentDate > oauthToken.expiryDate ) {
+    Logger.withTag('refreshOauthToken').info('Refresh is necessary for token:', {
+      oauthTokenId: oauthToken.id,
+      expiryDate: oauthToken.expiryDate
+    });
+
+    const recordId = oauthToken.id;
+    const oauth2Client = new google.auth.OAuth2(
+      APP_GOOGLE_OAUTH_CLIENT_ID,
+      APP_GOOGLE_OAUTH_CLIENT_SECRET,
+      APP_GOOGLE_OAUTH_CALLBACK_URL // e.g., http://localhost:3000/api/auth/google-oauth/callback
+    );
+  
+    oauth2Client.setCredentials({
+      access_token: oauthToken.accessToken,
+      refresh_token: oauthToken.refreshToken,
+    });
+  
+    const [{ credentials }] = await Promise.all([
+      oauth2Client.refreshAccessToken().catch((err) => {
+        Logger.withTag('refreshOauthToken').error('Error refreshing token: ' + err);
+        return {
+          credentials: {
+            access_token: null,
+            expiry_date: null,
+          },
+        };
+      }),
+  
+      // Delete kv caches for this user
+      deleteAllUserCaches(oauthToken.userId)
+    ]);
+  
+    if (credentials.access_token) {
+      const newExpiryDate = new Date(credentials.expiry_date || Date.now()),
+        lastRefreshedAt = currentDate;
+  
+      await db.oAuthToken.update({
+        where: {
+          id: recordId,
+        },
+        data: {
+          accessToken: credentials.access_token,
+          expiryDate: newExpiryDate,
+          lastRefreshedAt,
+        },
+      });
+  
+      return {
+        newExpiryDate,
+        lastRefreshedAt
+      };
+    }
+  }
+
+  return false;
+}
