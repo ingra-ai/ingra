6e0edfa21249137805adbe6929f29caefbb91ade
vadi <vadz77@hotmail.com>
2024-05-21 04:34:47 +0000

    Introduce swagger for marketplace at /api/swagger - TRIAL

---
+++ app/api/v1/marketplace/[[...paths]]/handlers/generateMarketplaceVmArgs.ts
@@ -0,0 +1,27 @@
+import { VmContextArgs } from "@app/api/utils/vm/generateVmContextArgs";
+import { FunctionArgument } from "@prisma/client";
+
+export function generateMarketplaceVmArgs( functionArguments: FunctionArgument[], requestArgs: Record<string, any> = {}) {
+  const { oauthTokens = '{}', profile = '{}', envVars = '{}', ...restOfRequestArgs } = requestArgs;
+  // Fill requestArgs with default values if not provided.
+  if (functionArguments) {
+    for (const arg of functionArguments) {
+      if ( requestArgs?.[arg.name] === undefined || requestArgs?.[arg.name] === '' || requestArgs?.[arg.name] === null ) {
+        if ( ( typeof arg.defaultValue === 'string' && arg.defaultValue.length ) || ( typeof arg.defaultValue === 'number' && !isNaN(arg.defaultValue) ) ) {
+          requestArgs[arg.name] = arg.defaultValue
+        }
+      }
+    }
+  }
+
+  const context: VmContextArgs = {
+    userVars: {
+      oauthTokens: JSON.parse(oauthTokens),
+      profile: JSON.parse(profile),
+      ...JSON.parse(envVars)
+    },
+    ...( restOfRequestArgs || {} )
+  };
+
+  return context;
+}
