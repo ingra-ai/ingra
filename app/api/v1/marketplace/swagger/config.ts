77a5e3ecc4242dcb59baf645ff3bf6268a0cead9
vadi <vadz77@hotmail.com>
2024-05-23 10:39:09 +0000

    Fix user swagger, hackish

--- app/api/v1/marketplace/swagger/config.ts
+++ app/api/v1/marketplace/swagger/config.ts
@@ -1,21 +1,13 @@
 import { getSwaggerSpec } from '@app/api/(internal)/swagger/config';
 import { APP_NAME, APP_OPENAI_MANIFEST_DESC_FOR_HUMAN } from '@lib/constants';
-import { generateMarketplaceOpenApiSchema } from './generateMarketplaceOpenApiSchema';
+// import { generateMarketplaceOpenApiSchema } from './generateMarketplaceOpenApiSchema';
 
 export const getMarketplaceSwaggerSpec = async () => {
   // Disable for now
-  const allMarketplaceFunctionsPaths = await generateMarketplaceOpenApiSchema();
-  // const allMarketplaceFunctionsPaths = await new Promise((resolve) => {
-  //   setTimeout(() => {
-  //     resolve({});
-  //   }, 2000);
-  // });
+  // const allMarketplaceFunctionsPaths = await generateMarketplaceOpenApiSchema();
 
   return getSwaggerSpec({
     title: `Marketplace | ${APP_NAME} Plugin API`,
-    description: APP_OPENAI_MANIFEST_DESC_FOR_HUMAN,
-    extraPaths: {
-      ...( allMarketplaceFunctionsPaths || {} )
-    },
+    description: APP_OPENAI_MANIFEST_DESC_FOR_HUMAN
   });
 };
