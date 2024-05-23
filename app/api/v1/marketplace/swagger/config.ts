e993dafa02208f9acf76e9ecab78d83306168387
vadi <vadz77@hotmail.com>
2024-05-23 03:25:50 +0000

    Tests fixing global types not exported in swagger #2

--- app/api/v1/marketplace/swagger/config.ts
+++ app/api/v1/marketplace/swagger/config.ts
@@ -1,6 +1,5 @@
 import { getSwaggerSpec } from '@app/api/(internal)/swagger/config';
 import { APP_NAME, APP_OPENAI_MANIFEST_DESC_FOR_HUMAN } from '@lib/constants';
-import * as stuff from '@app/api/v1/types/api-response';
 // import { generateMarketplaceOpenApiSchema } from '@v1/marketplace/functions/generateMarketplaceOpenApiSchema';
 
 export const getMarketplaceSwaggerSpec = async () => {
