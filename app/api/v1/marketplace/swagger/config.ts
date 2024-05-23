38502fc18bdc87c67fd6e7ed905ded0b9c5ddd64
vadi <vadz77@hotmail.com>
2024-05-23 03:17:50 +0000

    Tests fixing global types not exported in swagger

--- app/api/v1/marketplace/swagger/config.ts
+++ app/api/v1/marketplace/swagger/config.ts
@@ -1,10 +1,16 @@
 import { getSwaggerSpec } from '@app/api/(internal)/swagger/config';
 import { APP_NAME, APP_OPENAI_MANIFEST_DESC_FOR_HUMAN } from '@lib/constants';
+import * as stuff from '@app/api/v1/types/api-response';
 // import { generateMarketplaceOpenApiSchema } from '@v1/marketplace/functions/generateMarketplaceOpenApiSchema';
 
 export const getMarketplaceSwaggerSpec = async () => {
   // Disable for now
-  const allMarketplaceFunctionsPaths = {};
+  // const allMarketplaceFunctionsPaths = await generateMarketplaceOpenApiSchema();
+  const allMarketplaceFunctionsPaths = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({});
+    }, 2000);
+  });
 
   return getSwaggerSpec({
     title: `Marketplace | ${APP_NAME} Plugin API`,
--- app/api/v1/me/swagger/config.ts
+++ app/api/v1/me/swagger/config.ts
@@ -2,6 +2,7 @@ import { getSwaggerSpec } from '@app/api/(internal)/swagger/config';
 import { generateOpenApiSchema } from '@app/api/utils/functions/generateOpenApiSchema';
 import { AuthSessionResponse } from "@app/auth/session/types";
 import { APP_NAME, APP_OPENAI_MANIFEST_DESC_FOR_HUMAN } from '@lib/constants';
+import * as stuff from '@app/api/v1/types/api-response';
 
 export const getAuthSwaggerSpec = async (authSession: AuthSessionResponse) => {
   if ( !authSession ) {
