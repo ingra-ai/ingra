4f34a240c7692e1dc70e0dbb60a3d7736017224e
vadi <vadz77@hotmail.com>
2024-05-23 01:28:37 +0000

    This update includes the following changes:
     • Added a new file handlerWhitelists.ts that exports whitelist arrays for different HTTP methods.
     • Renamed the file deleteCache.ts to clearCaches.ts and updated the code accordingly.
     • Updated the index.ts file to import and export the new clearCaches handler.
     • Modified the route.ts file to check the whitelist before executing the handler functions for GET, POST, PUT, PATCH, and DELETE requests.
     • Updated the Swagger configuration file to include all API files in the app/api/v1 folder. These changes aim to implement whitelisting for certain routes and
       improve the structure and documentation of the API.

---
+++ app/api/v1/me/[[...paths]]/handlerWhitelists.ts
@@ -0,0 +1,16 @@
+export const WHITELIST_GET_ROUTES = [
+  'functions'
+];
+export const WHITELIST_POST_ROUTES = [
+  'functions'
+];
+export const WHITELIST_PUT_ROUTES = [
+  'functions'
+];
+export const WHITELIST_PATCH_ROUTES = [
+  'functions'
+];
+export const WHITELIST_DELETE_ROUTES = [
+  'functions',
+  'clearCaches'
+];
--- app/api/v1/me/[[...paths]]/handlers/index.ts
+++ app/api/v1/me/[[...paths]]/handlers/index.ts
@@ -1,2 +1,26 @@
 export { default as functions } from './functions';
-export { default as deleteCache } from './deleteCache';
+
+/**
+ * @swagger
+ * /api/v1/me/clearCaches:
+ *   delete:
+ *     summary: Clear caches for the current session
+ *     operationId: clearCaches
+ *     description: Clear all caches for the current session. This may fix some issues for OAuth credentials being expired, with a trade-off of next request will be slower.
+ *     responses:
+ *       '200':
+ *         description: Successfully retrieved list of tasks
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ApiSuccess'
+ *       '400':
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ApiError'
+ *     tags:
+ *       - Built-ins Internal
+ */
+export { default as clearCaches } from './clearCaches';
