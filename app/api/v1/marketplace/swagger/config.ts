33ba5e666692dd271126836daf48ef9940bd8b91
vadi <vadz77@hotmail.com>
2024-05-23 07:43:20 +0000

    File relocations to fix swagger not loading schema issue on /me

--- app/api/v1/marketplace/swagger/config.ts
+++ app/api/v1/marketplace/swagger/config.ts
@@ -1,6 +1,6 @@
 import { getSwaggerSpec } from '@app/api/(internal)/swagger/config';
 import { APP_NAME, APP_OPENAI_MANIFEST_DESC_FOR_HUMAN } from '@lib/constants';
-import { generateMarketplaceOpenApiSchema } from '@v1/marketplace/functions/generateMarketplaceOpenApiSchema';
+import { generateMarketplaceOpenApiSchema } from './generateMarketplaceOpenApiSchema';
 
 export const getMarketplaceSwaggerSpec = async () => {
   // Disable for now
