8d5ca0c3f77f7e9641befd4b12e66a42bbf55e0c
vadi <vadz77@hotmail.com>
2024-05-25 07:46:29 +0000

    Add workaround vercel issue with swagger js doc with kv

--- app/api/(internal)/swagger/config.ts
+++ app/api/(internal)/swagger/config.ts
@@ -6,41 +6,41 @@
 
 import { APP_NAME, APP_OPENAI_MANIFEST_DESC_FOR_HUMAN, APP_PACKAGE_VERSION } from '@lib/constants';
 import { createSwaggerSpec } from 'next-swagger-doc';
+import { kv } from '@vercel/kv';
 
 export type SwaggerOptions = NonNullable<Parameters<typeof createSwaggerSpec>[0]>;
 
-type GetSwaggerSpecProps = {
-  title?: string;
-  description?: string;
-  definition?: {
-    paths: SwaggerOptions['definition']['paths'];
-    components: SwaggerOptions['definition']['components'];
-  };
-};
+/**
+ * A bit of background story for this key.
+ * For some reason, swagger jsdoc fails to generate the annotations from files when accessed on /v1/me/swagger.
+ * The issue only happens when deployed on vercel, but works fine on local.
+ * Therefore, for the approach to work, we need to store the base swagger annotations in KV store, to keep the code cleaner.
+ * @todo Remove this key when the issue is resolved.
+ */
+export const BASE_SWAGGER_SPEC_KEY = 'BASE_SWAGGER_SPEC';
 
-export const getSwaggerSpec = ( extraOptions?: GetSwaggerSpecProps ) => {
+export const getSwaggerSpec = async () => {
   const swaggerOptions: SwaggerOptions = {
-    apiFolder: 'app/api/v1', // define api folder under app folder
+    apiFolder: 'api', // define api folder under app folder
     apis: [
-      'app/api/v1/**/*.ts'
+      'app/api/**/*.ts'
     ],
     definition: {
       openapi: '3.0.0',
       info: {
-        title: extraOptions?.title || `${APP_NAME} Plugin API`,
+        title: `${APP_NAME} Plugin API`,
         version: APP_PACKAGE_VERSION,
-        description: extraOptions?.description || APP_OPENAI_MANIFEST_DESC_FOR_HUMAN,
+        description: APP_OPENAI_MANIFEST_DESC_FOR_HUMAN,
       },
       paths: {
-        ...( extraOptions?.definition?.paths || {} ),
       },
       components: {
-        ...( extraOptions?.definition?.components || {} ),
       },
       security: [],
     },
   };
 
   const spec = createSwaggerSpec(swaggerOptions);
+  await kv.set(BASE_SWAGGER_SPEC_KEY, spec);
   return spec;
 };
