aed2afe040beb88671cf5b7e5aa274ce0882adc7
Choz <vadz77@hotmail.com>
2024-05-30 00:45:20 +1000

    Updated text copies and removed unused components (#8)
    * Refactored the login page in the app/auth/login/page.tsx file. The changes include:
    
     • Replaced the Lucide-React Shield icon with the Link icon.
     • Updated the title and descriptions of some FeatureCards.
     • Removed the IntroductionScreen component.
     • Updated the formatting and spacing of various elements.
     • Added a paragraph describing the purpose of the application. These changes aim to improve the visual appeal and clarity of the login page, as well as provide a better
       understanding of the application's features and functionality.
    
    * Removed package-lock.json

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,8 +3,7 @@ import Image from 'next/image';
 import { MagicLoginForm } from './MagicLoginForm';
 import { getAuthSession } from '@/app/auth/session';
 import { redirect, RedirectType } from 'next/navigation';
-import IntroductionScreen from './IntroductionScreen';
-import { Code, Cpu, Cloud, Rocket, Shield, Users } from 'lucide-react';
+import { Code, Cpu, Cloud, Rocket, Link, Users } from 'lucide-react';
 import { FeatureCard } from './FeatureCard';
 
 export default async function AuthLogin() {
@@ -19,13 +18,13 @@ export default async function AuthLogin() {
       <div className="col-span-1 lg:col-span-6">
         <div className={'flex flex-col items-center justify-center lg:min-h-screen lg:bg-gray-900 space-y-8 p-8 xl:p-12'}>
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <Image className="mx-auto h-12 w-12 xl:w-20 xl:h-20" src="/static/brand/bakabit-white-logo-only.svg" alt={ APP_NAME } width={96} height={96} />
+            <Image className="mx-auto h-12 w-12 xl:w-20 xl:h-20" src="/static/brand/bakabit-white-logo-only.svg" alt={APP_NAME} width={96} height={96} />
           </div>
-          <h1 className="text-xl font-bold my-4">Welcome to { APP_NAME }</h1>
+          <h1 className="text-xl font-bold my-4">Welcome to {APP_NAME}</h1>
           <MagicLoginForm />
           <div className="flex flex-col items-center space-y-4">
             <div className="text-xs text-center max-w-screen-md leading-6 text-muted-foreground">
-              <a href={ APP_LEGAL_PRIVACY_POLICY_URL } className="underline">Privacy Policy</a> | <a href={ APP_LEGAL_TOS_URL } className="underline">Terms of Service</a> | <a href={ APP_LEGAL_COOKPOL_URL } className="underline">Cookie Policy</a>
+              <a href={APP_LEGAL_PRIVACY_POLICY_URL} className="underline">Privacy Policy</a> | <a href={APP_LEGAL_TOS_URL} className="underline">Terms of Service</a> | <a href={APP_LEGAL_COOKPOL_URL} className="underline">Cookie Policy</a>
             </div>
             <p className="text-xs text-center max-w-screen-md leading-6 text-muted-foreground">
               © {new Date().getFullYear()} {APP_NAME}. All rights reserved.
@@ -35,11 +34,14 @@ export default async function AuthLogin() {
       </div>
 
       <div className="col-span-1 lg:col-span-6 flex flex-col justify-center items-center space-y-8 p-8 xl:p-12">
-        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-2 gap-8">
+        <p className="text-xs text-center max-w-[440px] leading-5 text-muted-foreground lg:order-2 mt-0 lg:mt-12">
+          {APP_NAME} is next-generation platform for curating and integrating custom automations. Imagine that you can have your own AI assistant in your pocket ready to do it for you. Let&apos;s automate stuffs.
+        </p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-2 gap-8 lg:order-1">
           <FeatureCard
             icon={<Code className="w-8 h-8 text-blue-500" />}
-            title="Function Marketplace"
-            description="Subscribe to a variety of pre-built functions or create your own to suit your needs."
+            title="Automation Marketplace"
+            description="Subscribe to a variety of pre-built functions for automation or create your own to suit your needs."
           />
           <FeatureCard
             icon={<Cpu className="w-8 h-8 text-green-500" />}
@@ -49,7 +51,7 @@ export default async function AuthLogin() {
           <FeatureCard
             icon={<Cloud className="w-8 h-8 text-yellow-500" />}
             title="Self-Generating Functions"
-            description="Leverage built-in functions to create new automations, enhanced by AI."
+            description="Leverage functions to create new automations, or using built-in functions to create even more functions or to improve itself, enhanced by AI."
           />
           <FeatureCard
             icon={<Rocket className="w-8 h-8 text-red-500" />}
@@ -57,9 +59,9 @@ export default async function AuthLogin() {
             description="Fork and customize functions to your needs, with the ability to scale effortlessly."
           />
           <FeatureCard
-            icon={<Shield className="w-8 h-8 text-purple-500" />}
-            title="Enhanced Security"
-            description="Experience top-tier security protocols ensuring your data and automations are safe and private."
+            icon={<Link className="w-8 h-8 text-orange-500" />}
+            title="Integration Flexibility"
+            description="Easily connect with a wide range of third-party services and APIs by utilizing environment variables, webhooks, or custom integrations."
           />
           <FeatureCard
             icon={<Users className="w-8 h-8 text-teal-500" />}
@@ -67,9 +69,6 @@ export default async function AuthLogin() {
             description="Join a growing community of users and developers for support, ideas, and collaboration."
           />
         </div>
-        <p className="text-xs text-center max-w-screen-md leading-6 text-muted-foreground">
-          { APP_NAME } is attempting to be next-generation platform for curating and integrating custom automations. Imagine that you can have your own AI assistant, Let&apos;s funcs the world.
-        </p>
       </div>
     </div>
   );
