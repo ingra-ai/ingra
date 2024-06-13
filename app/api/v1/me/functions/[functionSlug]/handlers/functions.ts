fa606109f67125e5422565357bd7e09a2f236e48
vadi <vadz77@hotmail.com>
2024-06-13 21:47:48 +1000

    Enable API endpoints for subscribed collections, and integrated it with openapi.json

--- app/api/v1/me/functions/[functionSlug]/handlers/functions.ts
+++ app/api/v1/me/functions/[functionSlug]/handlers/functions.ts
@@ -8,9 +8,10 @@ import { runUserFunction } from "@app/api/utils/functions/runUserFunction";
 const handlerFn = async ( functionSlug: string, requestArgs: Record<string, any> = {} ) => {
   return await apiAuthTryCatch<any>(async (authSession) => {
     if ( typeof functionSlug === 'string' && functionSlug.length) {
-      const userId = authSession.user.id;
+      const userId = authSession.user.id,
+        loggerObj = Logger.withTag('me-functions').withTag(`user:${ userId }`).withTag(`functionSlug:${ functionSlug }`);
 
-      Logger.withTag('me-functions').withTag(`user:${ userId }`).info(`Starts executing function: ${functionSlug}`, requestArgs);
+      loggerObj.info(`Starts executing function: ${functionSlug}`, requestArgs);
 
       const functionRecord = await db.function.findUnique({
         where: {
@@ -28,18 +29,18 @@ const handlerFn = async ( functionSlug: string, requestArgs: Record<string, any>
       });
 
       if ( !functionRecord ) {
-        throw new ActionError('error', 400, `Function not found.`);
+        throw new ActionError('error', 404, `Function not found.`);
       }
 
       const { result, metrics, errors } = await runUserFunction(authSession, functionRecord, requestArgs);
 
       if ( errors.length ) {
         const errorMessage = errors?.[0].message || 'An error occurred while executing the function.';
-        Logger.withTag('me-functions').withTag(`user:${ userId }`).error(`Errored executing function: ${errorMessage}`);
+        loggerObj.error(`Errored executing function: ${errorMessage}`);
         throw new ActionError('error', 400, errorMessage);
       }
 
-      Logger.withTag('me-functions').withTag(`user:${ authSession.user.id }`).info(`Finished executing function: ${functionSlug}`, metrics);
+      loggerObj.info(`Finished executing function: ${functionSlug}`, metrics);
 
       return NextResponse.json(
         {
