4539129282401d8b07ef35670904251cadfd52e4
Choz <vadz77@hotmail.com>
2024-08-28 20:50:19 +1000

    Turborepo migration - Updated readme and fix env build (#39)

--- vercel-build-hubs.sh
+++ vercel-build-hubs.sh
@@ -2,9 +2,9 @@
 
 if [[ $VERCEL_GIT_COMMIT_REF == "main" ]]; then
   echo "Deploying on 'main' branch"
-  cp .env.example .env
-  pnpm test
-  pnpm build
+  cp apps/hubs/.env.example apps/hubs/.env
+  pnpm test -F hubs
+  pnpm build -F hubs
 else
   echo "Invalid branch. Cancelling build."
   exit 0
