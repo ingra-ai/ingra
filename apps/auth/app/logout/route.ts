df535017972175122475ff25c514c4ae63d719e3
vadi <vadz77@hotmail.com>
2024-09-02 01:34:06 +1000

    feat: Enhance logout functionality and update landing page URL
     - Improved logout process by integrating `NextRequest` to handle cookies.
     - Added `getAuthSession` to fetch the current authentication session.
     - Updated database to remove active session using both JWT and userId.
     - Cleared authentication caches upon logout.
     - Enhanced logging to include censored user email.
     - Updated the landing page URL to `/overview/dashboard`.

--- apps/auth/app/logout/route.ts
+++ apps/auth/app/logout/route.ts
@@ -2,37 +2,47 @@
 import { APP_SESSION_COOKIE_NAME } from '@repo/shared/lib/constants';
 import { cookies } from 'next/headers';
 import { APP_AUTH_LOGIN_URL } from '@repo/shared/lib/constants';
-import { RedirectType, redirect } from 'next/navigation';
 import db from '@repo/db/client';
 import { Logger } from '@repo/shared/lib/logger';
+import { clearAuthCaches } from '@repo/shared/data/auth/session/caches';
+import { NextRequest } from 'next/server';
+import { censorEmail } from '@repo/shared/lib/utils';
+import { redirect, RedirectType } from 'next/navigation';
+import { getAuthSession } from '@repo/shared/data/auth/session';
 
-export async function GET() {
-  const cookieStore = cookies();
-  const jwtCookie = cookieStore.get(APP_SESSION_COOKIE_NAME);
+export async function GET(request: NextRequest) {
+  const jwtCookie = request.cookies.get(APP_SESSION_COOKIE_NAME);
+  const authSession = await getAuthSession();
 
-  if (jwtCookie?.value) {
+  if (authSession && jwtCookie?.value) {
     try {
       // Update db to remove active session
-      await db.activeSession.update({
+      const expireActiveSession = db.activeSession.update({
         where: {
           jwt: jwtCookie.value,
+          userId: authSession?.userId
         },
         data: {
           expiresAt: new Date(0),
         },
       });
+
+      // Remove session cookies
+      const cookieStore = cookies();
+      cookieStore.delete(APP_SESSION_COOKIE_NAME);
+      request.cookies.delete(APP_SESSION_COOKIE_NAME);
+
+      await Promise.all([ 
+        expireActiveSession, 
+        clearAuthCaches( authSession ) 
+      ]);
+      
+      Logger.withTag('action|logout').info(`${ censorEmail( authSession.user.email ) } has logged out.`);
     } catch (error: any) {
       Logger.withTag('action|logout').error('Error removing session due to logout', { jwt: jwtCookie.value, error });
     }
-
-    // Remove from redis
-    Logger.withTag('action|logout').log('Session removed due to logout', {
-      jwt: jwtCookie.value,
-    });
-
-    // Remove session cookies
-    cookieStore.delete(APP_SESSION_COOKIE_NAME);
   }
 
+  // Redirect to the app
   redirect(APP_AUTH_LOGIN_URL, RedirectType.replace);
 }
