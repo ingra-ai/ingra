66ce67e6c3869cdc25d69db18f2ac772639ff2f2
vadi <vadz77@hotmail.com>
2024-09-14 00:50:11 +1000

    feat: Refactor components and update dependencies
     • Refactor Leftbar, Navbar, Anchor, and other components to improve modularity and reusability.
     • Move components from apps/docs to packages/components for better code organization.
     • Update imports to reflect new component paths.
     • Add new props to Navbar and Leftbar for dynamic rendering of children and navlinks.
     • Remove next-themes dependency from apps/docs and add it to packages/components.
     • Update pnpm-lock.yaml to reflect changes in dependencies and optional dependencies.
     • Delete unused favicon.ico file from apps/docs.
     • Enhance RootLayout in apps/hubs with ThemeProvider and metadata updates.

--- apps/docs/app/docs/layout.tsx
+++ apps/docs/app/docs/layout.tsx
@@ -1,4 +1,5 @@
-import { Leftbar } from '@/components/leftbar';
+import { Leftbar } from '@repo/components/navs/leftbar';
+import DocsMenu from '@/components/docs-menu';
 
 export default function DocsLayout({
   children,
@@ -7,7 +8,9 @@ export default function DocsLayout({
 }>) {
   return (
     <div className="flex items-start gap-14">
-      <Leftbar key="leftbar" />
+      <Leftbar key="leftbar">
+        <DocsMenu />
+      </Leftbar>
       <div className="flex-[4]">{children}</div>{' '}
     </div>
   );
---
+++ packages/components/src/navs/types.ts
@@ -0,0 +1,23 @@
+
+export type Navlink = {
+  title: string;
+  href: string;
+  external?: boolean;
+};
+
+export type NavbarProps = {
+  navlinks?: Navlink[];
+  sheetChildren?: React.ReactNode;
+};
+
+export type NavMenuProps = {
+  navlinks: Navlink[];
+  isSheet?: boolean;
+};
+
+export type LeftbarProps = {
+};
+
+export type SheetLeftbarProps = {
+  navlinks: Navlink[];
+};
