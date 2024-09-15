e3c8322c9d0b468e54d9e5c44a59a51250f11dc6
vadi <vadz77@hotmail.com>
2024-09-16 03:09:43 +1000

    feat: Update note component styles and add new documentation sections
     - Updated `note.tsx` component styles for `danger`, `warning`, and `success` types.
     - Added new documentation files for curating functions via ChatGPT and Web UI.
     - Introduced a comprehensive guide on built-in functions in `builtin-functions/index.mdx`.
     - Enhanced Google API integration documentation with updated note content.
     - Expanded the introduction section with details on how Ingra works.
     - Added new quick start guide sections for account setup, ChatGPT setup, and LangChain setup.
     - Implemented a new `rehypeReplaceVariables` plugin to replace constants in markdown files.
     - Updated routes configuration to include new documentation sections.
     - Removed unused public images.

--- apps/docs/components/note.tsx
+++ apps/docs/components/note.tsx
@@ -10,9 +10,9 @@ type NoteProps = PropsWithChildren & {
 export default function Note({ children, title = 'Note', type = 'note' }: NoteProps) {
   const noteClassNames = clsx({
     'dark:bg-neutral-900 bg-neutral-100': type == 'note',
-    'dark:bg-red-950 bg-red-100 border-red-200 dark:border-red-900': type === 'danger',
-    'dark:bg-orange-950 bg-orange-100 border-orange-200 dark:border-orange-900': type === 'warning',
-    'dark:bg-green-950 bg-green-100 border-green-200 dark:border-green-900': type === 'success',
+    'dark:bg-red-900 bg-red-100 border-red-200 dark:border-red-900': type === 'danger',
+    'bg-info text-info-foreground': type === 'warning',
+    'dark:bg-green-900 bg-green-100 border-green-200 dark:border-green-900': type === 'success',
   });
 
   return (
