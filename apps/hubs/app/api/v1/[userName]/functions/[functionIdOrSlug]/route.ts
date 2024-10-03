affdacaf514feeb31a6f489fb36695a7de6ee95c
vadi <vadz77@hotmail.com>
2024-10-03 10:52:01 +1000

    feat: Update access type handling in function routes
     • Refactor access type from a single FunctionAccessType to an array FunctionAccessType[] to support multiple access levels.
     • Adjust logic to set accessTypes based on user ownership or subscription status.
     • Ensure accessTypes is correctly passed to getFunctionAccessibleByUser and logFunctionExecution.
     • Enhance flexibility in determining user access, allowing for more nuanced permission checks.
     • Improve code consistency across different routes by aligning access type handling.

--- apps/hubs/app/api/v1/[userName]/functions/[functionIdOrSlug]/route.ts
+++ apps/hubs/app/api/v1/[userName]/functions/[functionIdOrSlug]/route.ts
@@ -31,7 +31,7 @@ async function handlerFn(args: HandlerArgs) {
 
     loggerObj.info(`Starts executing function: ${functionIdOrSlug}`, requestArgs);
 
-    let accessType: FunctionAccessType = 'owner';
+    let accessTypes: FunctionAccessType[] = ['owner'];
 
     if (!functionIdOrSlug) {
       throw new ActionError('error', 400, 'Function ID or slug is required to run this method.');
@@ -39,15 +39,15 @@ async function handlerFn(args: HandlerArgs) {
 
     if (authSession.user.profile?.userName === userName) {
       // If the user is the owner of the function
-      accessType = 'owner';
+      accessTypes = 'owner';
     } else {
       // User is probably a subscriber
-      accessType = 'subscriber';
+      accessTypes = ['subscriber', 'subscribedCollection'];
     }
 
     // Find the function
     const functionRecord = await getFunctionAccessibleByUser(authSession.user.id, functionIdOrSlug, {
-      accessTypes: [accessType],
+      accessTypes,
       findFirstArgs: {
         include: {
           arguments: true,
@@ -74,7 +74,7 @@ async function handlerFn(args: HandlerArgs) {
       functionId: functionRecord.id,
       functionIdOrSlug: functionIdOrSlug,
       isSandboxDebug,
-      accessType,
+      accessTypes,
       metrics,
       errors,
     });
