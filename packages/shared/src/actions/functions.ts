3e9d38ebccbab169afe2aae8f0cb2ae32a290059
vadi <vadz77@hotmail.com>
2024-10-09 23:27:22 +1100

    feat: Add publish/unpublish functionality for functions
     • Introduced a new feature to toggle the publish status of functions, allowing users to publish or unpublish their functions.
     • Updated FunctionCard component to include a button for toggling the publish status, with visual indicators for published and unpublished states.
     • Modified FunctionSearchList to handle the publish toggle action and provide user feedback through toast notifications.
     • Enhanced badge component with new variants for success, warning, and info states.
     • Refactored actions to include togglePublishFunction for managing the publish status in the backend.
     • Removed redundant subscription toggle logic for clarity and maintainability.

--- packages/shared/src/actions/functions.ts
+++ packages/shared/src/actions/functions.ts
@@ -11,8 +11,7 @@ import {
   deleteFunction as dataDeleteFunctions,
   cloneFunction as dataCloneFunction,
   subscribeToFunction as dataSubscribeToFunction,
-  unsubscribeToFunction as dataUnsubscribeToFunction,
-  toggleFunctionSubscription,
+  unsubscribeToFunction as dataUnsubscribeToFunction
 } from '../data/functions';
 import { addFunctionToCollection, removeFunctionFromCollection } from '../data/collections';
 import { getUserRepoFunctionsEditUri } from '../lib/constants/repo';
@@ -22,6 +21,11 @@ export const upsertFunction = async (values: z.infer<typeof FunctionSchema>) => 
   const { data } = validatedValues;
 
   return await actionAuthTryCatch(async (authSession) => {
+    // Validate username
+    if (!authSession.user.profile?.userName) {
+      throw new ActionError('error', 400, 'Profile username is not setup.');
+    }
+
     const result = await dataUpsertFunctions(data, authSession.user.id);
 
     return {
@@ -74,6 +78,10 @@ export const cloneFunction = async (functionId: string) => {
     const clonedFunction = await dataCloneFunction(functionId, authSession.user.id),
       ownerUsername = authSession.user.profile?.userName;
 
+    if ( !ownerUsername ) {
+      throw new ActionError('error', 400, 'Profile username is not setup.');
+    }
+
     return {
       status: 'ok',
       message: `Function "${clonedFunction.slug}" has been cloned!`,
@@ -94,8 +102,50 @@ export const cloneFunction = async (functionId: string) => {
   });
 };
 
+export const togglePublishFunction = async (functionId: string) => {
+  return await actionAuthTryCatch(async (authSession) => {
+    const functionRecord = await db.function.findUnique({
+      where: {
+        id: functionId,
+        ownerUserId: authSession.user.id,
+      },
+    });
+
+    if (!functionRecord) {
+      throw new ActionError('error', 404, 'Function not found');
+    }
+
+    const result = await db.function.update({
+      where: {
+        id: functionRecord.id,
+      },
+      data: {
+        isPublished: !functionRecord.isPublished,
+      },
+    });
+
+    return {
+      status: 'ok',
+      message: `Function "${functionRecord.slug}" has been ${result.isPublished ? 'published' : 'unpublished'}!`,
+      data: result,
+    };
+  })
+  .catch((error) => {
+    return {
+      status: error?.status || 'error',
+      message: error?.message || `Failed to publish function!`,
+      data: null,
+    };
+  });
+}
+
 export const subscribeToFunction = async (functionId: string) => {
   return await actionAuthTryCatch(async (authSession) => {
+    // Validate username
+    if (!authSession.user.profile?.userName) {
+      throw new ActionError('error', 400, 'Profile username is not setup.');
+    }
+
     const record = await dataSubscribeToFunction(functionId, authSession.user.id);
 
     return {
@@ -115,6 +165,11 @@ export const subscribeToFunction = async (functionId: string) => {
 
 export const unsubscribeToFunction = async (functionId: string) => {
   return await actionAuthTryCatch(async (authSession) => {
+    // Validate username
+    if (!authSession.user.profile?.userName) {
+      throw new ActionError('error', 400, 'Profile username is not setup.');
+    }
+
     const record = await dataUnsubscribeToFunction(functionId, authSession.user.id);
 
     return {
@@ -127,25 +182,6 @@ export const unsubscribeToFunction = async (functionId: string) => {
     return {
       status: error?.status || 'error',
       message: error?.message || `Failed to unsubscribe to function!`,
-      data: null,
-    };
-  });
-};
-
-export const subscribeToggleFunction = async (functionId: string) => {
-  return await actionAuthTryCatch(async (authSession) => {
-    const subscribedFunction = await toggleFunctionSubscription(functionId, authSession.user.id);
-
-    return {
-      status: 'ok',
-      message: subscribedFunction.isSubscribed ? `Function "${subscribedFunction.functionSlug}" has been subscribed!` : `Function "${subscribedFunction.functionSlug}" has been unsubscribed!`,
-      data: subscribedFunction,
-    };
-  })
-  .catch((error) => {
-    return {
-      status: error?.status || 'error',
-      message: error?.message || `Failed to perform operation!`,
       data: null,
     };
   });
