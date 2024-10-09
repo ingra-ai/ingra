437228ffff88511b80a0305340ee170e3994b5ba
vadi <vadz77@hotmail.com>
2024-10-10 01:15:38 +1100

    Commit Message
    feat: Refactor 404 pages and enhance UI consistency
    
     • Introduced a new Simple404 component for consistent 404 error pages across the application.
     • Replaced individual 404 page implementations with the Simple404 component in various app modules.
     • Updated OAuthList.tsx and other components to improve dark mode support and UI consistency.
     • Replaced TopSubNav with SideNavItem for better navigation in settings layout.
     • Added SideNavItem component for side navigation in settings.
     • Updated package.json to include the new Simple404 component path.
     • Enhanced accessibility and readability of text elements in settings and profile pages.

--- apps/hubs/app/(protected)/settings/profile/page.tsx
+++ apps/hubs/app/(protected)/settings/profile/page.tsx
@@ -31,8 +31,8 @@ export default async function Page() {
     <div className="container mx-auto">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-x-16 py-16 w-full">
         <div className="md:col-span-1 text-right">
-          <h2 className="text-base font-semibold leading-7 text-white">Personal Information</h2>
-          <p className="text-sm mt-2 leading-6 text-gray-400">
+          <h2 className="text-base font-semibold leading-7 text-gray-900 dark:text-gray-100">Personal Information</h2>
+          <p className="text-sm mt-2 leading-6 text-gray-600 hover:text-gray-400">
             View and update your personal details, including your name, and timezone. Keeping your information up-to-date ensures that you receive important notifications and improves your user experience.
           </p>
         </div>
@@ -43,8 +43,8 @@ export default async function Page() {
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-x-8 py-16 w-full">
         <div className="md:col-span-1 text-right">
-          <h2 className="text-base font-semibold leading-7 text-white">Log out other sessions</h2>
-          <p className="text-sm mt-2 leading-6 text-gray-400">
+          <h2 className="text-base font-semibold leading-7 text-gray-900 dark:text-gray-100">Log out other sessions</h2>
+          <p className="text-sm mt-2 leading-6 text-gray-600 hover:text-gray-400">
             Secure your account by logging out of all other active sessions. This action will terminate access from all devices except the one you are currently using, protecting your information from unauthorized access.
           </p>
         </div>
@@ -53,7 +53,7 @@ export default async function Page() {
           <div className="block">
             <TooltipProvider>
               <Tooltip>
-                <TooltipTrigger type="button" className="rounded-md bg-gray-500 px-3 py-2 text-sm font-semibold text-white shadow-sm">
+                <TooltipTrigger type="button" className="rounded-md bg-gray-500 px-3 py-2 text-sm font-semibold text-gray-900 dark:text-gray-100 shadow-sm">
                   Log out other sessions
                 </TooltipTrigger>
                 <TooltipContent>
@@ -67,8 +67,8 @@ export default async function Page() {
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-x-8 py-16 w-full">
         <div className="md:col-span-1 text-right">
-          <h2 className="text-base font-semibold leading-7 text-white">Delete account</h2>
-          <p className="text-sm mt-2 leading-6 text-gray-400">
+          <h2 className="text-base font-semibold leading-7 text-gray-900 dark:text-gray-100">Delete account</h2>
+          <p className="text-sm mt-2 leading-6 text-gray-600 hover:text-gray-400">
             Permanently delete your account and all associated data. This action cannot be undone. If you proceed, your personal information, preferences, and history will be irretrievably removed in accordance with our data retention
             policy
           </p>
---
+++ apps/hubs/components/navs/SideNavItem.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { Button } from '@repo/components/ui/button';
+import Link from 'next/link';
+import { cn } from '@repo/shared/lib/utils';
+import { usePathname } from 'next/navigation';
+
+interface SideNavItemProps {
+  item: {
+    href: string;
+    name: string;
+  };
+}
+
+export default function SideNavItem({ item }: SideNavItemProps) {
+  const pathname = usePathname(),
+    isActive = pathname.includes(item.href);
+
+  return (
+    <Button
+      variant={isActive ? "secondary" : "ghost"}
+      className={cn(
+        "w-full justify-start gap-2 px-2",
+        isActive
+          ? "bg-secondary text-secondary-foreground"
+          : "hover:bg-accent hover:text-accent-foreground",
+        isActive && "font-semibold"
+      )}
+      asChild
+    >
+      <Link href={item.href}>
+        {item.name}
+      </Link>
+    </Button>
+  )
+}
