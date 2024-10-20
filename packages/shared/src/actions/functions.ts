6bd3f9df0d579723c953a27a40cb19694faec6af
vadi <vadz77@hotmail.com>
2024-10-20 21:52:19 +1100

    refactor: Simplify function creation logic and improve error handling
    This refactor streamlines the createNewFunction logic by moving validation and record creation to a shared utility, reducing redundancy. It removes unnecessary imports and logic from
    route.ts, centralizing function creation in functions.ts. Error handling is improved by throwing specific errors for invalid payloads and missing user profiles. The refactor enhances
    maintainability and readability without altering functionality.

--- packages/shared/src/actions/functions.ts
+++ packages/shared/src/actions/functions.ts
@@ -1,20 +1,150 @@
 'use server';
 
 import * as z from 'zod';
-import { ActionError } from '../types/api-response';
-import { FunctionSchema } from '../schemas/function';
-import { validateAction } from '../lib/action-helpers';
+import { ActionError } from '@repo/shared/types';
+import { FunctionArgumentSchema, FunctionSchema, FunctionTagsSchema } from '@repo/shared/schemas/function';
+import { validateAction } from '@repo/shared/lib/action-helpers';
 import db from '@repo/db/client';
-import { actionAuthTryCatch } from '../utils/actionAuthTryCatch';
+import { actionAuthTryCatch } from '@repo/shared/utils/actionAuthTryCatch';
 import {
   upsertFunction as dataUpsertFunctions,
   deleteFunction as dataDeleteFunctions,
   cloneFunction as dataCloneFunction,
   subscribeToFunction as dataSubscribeToFunction,
-  unsubscribeToFunction as dataUnsubscribeToFunction
-} from '../data/functions';
-import { addFunctionToCollection, getCollectionAccessibleByUser, removeFunctionFromCollection } from '../data/collections';
-import { getUserRepoFunctionsEditUri } from '../lib/constants/repo';
+  unsubscribeToFunction as dataUnsubscribeToFunction,
+} from '@repo/shared/data/functions';
+import { addFunctionToCollection, removeFunctionFromCollection } from '@repo/shared/data/collections';
+import { getUserRepoFunctionsEditUri } from '@repo/shared/lib/constants/repo';
+import isNil from 'lodash/isNil';
+import isBoolean from 'lodash/isBoolean';
+import { generateUserVars } from '@repo/shared/utils/vm/generateUserVars';
+import { generateCodeDefaultTemplate } from '@repo/shared/utils/vm/functions/generateCodeDefaultTemplate';
+
+export const createNewFunction = async (functionPayload: z.infer<typeof FunctionSchema>, collectionRecordIdOrSlug?: string) => {
+  return await actionAuthTryCatch(async (authSession) => {
+    if (!functionPayload || typeof functionPayload !== 'object' || Object.keys(functionPayload).length === 0) {
+      throw new Error('Function payload is empty or invalid. Are you passing the patch data as "{ function: { ... } }"?');
+    }
+  
+    if (functionPayload.id) {
+      throw new Error("Creating new function doesn't require an ID. Please remove the ID from the function schema.");
+    }
+  
+    if (isNil(functionPayload.slug.trim())) {
+      throw new Error('Function slug is required to create a new function.');
+    }
+  
+    if (isNil(functionPayload.description.trim())) {
+      throw new Error('Function description is required to create a new function.');
+    }
+
+    // Validate username
+    if (!authSession.user.profile?.userName) {
+      throw new ActionError('error', 400, 'Profile username is not setup.');
+    }
+    
+    const providedFields: (keyof z.infer<typeof FunctionSchema>)[] = ['slug', 'description'];
+
+    /**
+     * Populate skeleton function record with user and environment variables as code template.
+     */
+    const optionalEnvVars = authSession.user.envVars.map((envVar) => ({
+        id: envVar.id,
+        ownerUserId: envVar.ownerUserId,
+        key: envVar.key,
+        value: envVar.value,
+      })),
+      userVarsRecord = generateUserVars(authSession),
+      allUserAndEnvKeys = Object.keys(userVarsRecord).concat(optionalEnvVars.map((envVar) => envVar.key));
+
+    const safeFunctionRecord = {
+      slug: functionPayload.slug.trim(),
+      code: generateCodeDefaultTemplate(allUserAndEnvKeys),
+      isPrivate: true,
+      isPublished: true,
+      httpVerb: 'GET',
+      description: functionPayload.description.trim(),
+      arguments: [] as z.infer<typeof FunctionArgumentSchema>[],
+      tags: [] as z.infer<typeof FunctionTagsSchema>[],
+    } as z.infer<typeof FunctionSchema>;
+
+    /**
+     * Populate all the provided fields from the payload. If the field is not provided, it will be populated with the default value.
+     */
+    const acceptableFieldNames: (keyof z.infer<typeof FunctionSchema>)[] = [
+      'code', 
+      'httpVerb', 
+      'isPrivate', 
+      'isPublished', 
+      'arguments', 
+      'tags'
+    ];
+
+    /**
+     * Iterate through all the acceptable field names and populate the fields that are provided in the payload.
+     */
+    acceptableFieldNames.forEach((acceptableFieldName) => {
+      // Check if the field is provided in the payload
+      if (Object.prototype.hasOwnProperty.call(functionPayload, acceptableFieldName)) {
+        // Add the field to the provided fields list
+        providedFields.push(acceptableFieldName);
+
+        // Populate all the fields that are provided
+        switch (acceptableFieldName) {
+          case 'code':
+            safeFunctionRecord[acceptableFieldName] = functionPayload[acceptableFieldName];
+            break;
+          case 'httpVerb':
+            safeFunctionRecord[acceptableFieldName] = functionPayload[acceptableFieldName];
+            break;
+          case 'isPrivate':
+          case 'isPublished':
+            safeFunctionRecord[acceptableFieldName] = isBoolean(functionPayload[acceptableFieldName]) ? functionPayload[acceptableFieldName] : safeFunctionRecord[acceptableFieldName];
+            break;
+          case 'arguments':
+            safeFunctionRecord.arguments = (functionPayload.arguments || [])
+              .filter((elem) => elem.name.trim())
+              .map((elem) => {
+                const newArgument: z.infer<typeof FunctionArgumentSchema> = {
+                  functionId: '',
+                  name: elem.name,
+                  type: elem.type,
+                  defaultValue: elem.defaultValue,
+                  description: elem.description,
+                  isRequired: elem.isRequired,
+                };
+
+                return newArgument;
+              });
+            break;
+          case 'tags':
+            safeFunctionRecord.tags = (functionPayload.tags || [])
+              .filter((elem) => elem.name.trim())
+              .map((elem) => {
+                const newTag: z.infer<typeof FunctionTagsSchema> = {
+                  functionId: '',
+                  name: elem.name,
+                };
+
+                return newTag;
+              });
+            break;
+          default:
+            break;
+        }
+      }
+    });
+
+    const { data } = await validateAction(FunctionSchema, safeFunctionRecord);
+    const result = await dataUpsertFunctions(data, authSession.user.id, collectionRecordIdOrSlug);
+
+    return {
+      status: 'ok' as const,
+      message: `Function "${result.slug}" has been created.`,
+      data: result,
+    };
+  });
+}
 
 export const upsertFunction = async (values: z.infer<typeof FunctionSchema>, collectionRecordIdOrSlug?: string) => {
   const validatedValues = await validateAction(FunctionSchema, values);
