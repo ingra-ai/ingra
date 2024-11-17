107a1c82dee807e9839cec7f1c2aa3d95bd5ec2e
vadi <vadz77@hotmail.com>
2024-11-17 21:24:00 +1100

    refactor: Remove console.log and clean up styles in components
     - Removed unnecessary console.log from CollectionDetailView to clean up code.
     - Simplified className string in Simple404 for better readability and maintenance.

--- packages/components/src/not-found/Simple404.tsx
+++ packages/components/src/not-found/Simple404.tsx
@@ -2,7 +2,7 @@
 
 export default function Simple404() {
   return (
-    <div className={`relative flex min-h-screen flex-col items-center justify-center bg-gray-100 dark:bg-gray-900`} data-testid="page-not-found">
+    <div className={`relative flex min-h-screen flex-col items-center justify-center`} data-testid="page-not-found">
       <div className="flex items-center space-x-5 text-gray-800 dark:text-gray-200">
         <span className="text-2xl font-light">404</span>
         <span className="text-4xl font-extralight">|</span>
