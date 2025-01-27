91fb2b71bacc241be993983e55c4926dd6b1bef8
vadi <vadz77@hotmail.com>
2025-01-27 20:21:53 +1100

    feat: Add /whoami route to return authentication status
     Implemented a new GET endpoint in the API to check if the user is authenticated and return a success message along with the user's session data.

---
+++ apps/hubs/app/api/v1/me/(built-ins)/whoami/route.ts
@@ -0,0 +1,17 @@
+import { apiAuthTryCatch } from '@repo/shared/utils/apiAuthTryCatch';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  return await apiAuthTryCatch<any>(async (authSession) => {
+    return NextResponse.json(
+      {
+        status: 'success',
+        message: `You are authenticated.`,
+        data: result,
+      },
+      {
+        status: 200,
+      }
+    );
+  });
+}
